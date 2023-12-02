260c62bf00324d2c99f9a0c85ac4f766111a8981
Testify <tvphus@outlook.com>
2023-12-03 00:25:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -36,26 +36,6 @@ client.on('interactionCreate', (interaction) => {
   }
 });
 
-const question = (q) => new Promise((resolve) => rl.question(q, resolve));
-(async ()=>{
-  const token = await question('Application token? ');
-  if(!token) throw new Error('Invalid token');
-
-  const ratelimitTest = await fetch(`https://discord.com/api/v9/invites/discord-developers`);
-
-  if(!ratelimitTest.ok) {
-    await question(`Uh oh, looks like the node you're on is currently being blocked by Discord. Press the "Enter" button on your keyboard to be reassigned to a new node. (you'll need to rerun the program once you reconnect)`)
-
-    // This kills the container manager on the repl forcing Replit to assign the repl to another node with another IP address (if the ip is globally rate limited)
-    //^ in short: Restarts the bot to be used again/attempted to start up again!
-    execSync('kill 1');
-    return;
-  };
-  
-  await client.login(token).catch((err) => {
-    throw err
-  });
-
   await client.rest.put(Routes.applicationCommands(client.user.id), { body: commands });
 
   console.log('DONE | Application/Bot is up and running. DO NOT CLOSE THIS TAB UNLESS YOU ARE FINISHED USING THE BOT, IT WILL PUT THE BOT OFFLINE.');
