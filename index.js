0f6eb2c1320c455a70115c21f7a4921f7fce8fb1
Testify <tvphus@outlook.com>
2025-10-17 02:15:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,32 +8,40 @@ const ping = {
   description: 'Pings the bot and shows the latency'
 };
 
-
 // Command Example
-const tvp = {
-  name:'tvp',
-  description:'Example'
+const miyuko = {
+  name:'miyukowo',
+  description:'say hello to miyukowo'
 };
 
-const commands = [ping, tvp]; // Add your commands with commas to add them to the bot!
+const commands = [ping, miyuko];
 
 const client = new Client({ intents: [] });
 const rl = createInterface({ input: process.stdin, output: process.stdout });
 
-
 client.on('interactionCreate', (interaction) => {
   if (interaction.commandName === 'ping') {
     interaction.reply(`Latency is ${Date.now() - interaction.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);  
-  } else if(interaction.commandName === 'tvp') { // This is the example command's name!
-    interaction.reply('whatup');
-  } else { // a response if you forget to add the command here
+  } else if(interaction.commandName === 'tvp') {
+    interaction.reply(`whatup <@${interaction.user.id}>!`);
+  }
+  } else {
     interaction.reply('this command\'s response has not been added yet!');
   }
 });
 
 client.login(process.env.TOKEN);
 
-client.on('ready', () => {
+client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  
+  try {
+    console.log('Started refreshing application (/) commands.');
+    await client.application.commands.set(commands);
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error('Error registering commands:', error);
+  }
+  
   console.log('DONE! Application/Bot is up and running.');
-  });
+});
